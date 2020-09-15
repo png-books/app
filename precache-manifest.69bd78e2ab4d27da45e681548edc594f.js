ad9438ed21ee296262fee14398c74787a57eea55
Rebecca Bol <rebeccabol@Rebeccas-iMac.local>
2020-09-15 12:21:40 -0700

    Updates

---
+++ precache-manifest.69bd78e2ab4d27da45e681548edc594f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "114679f96943f4e832fa88c3ff884945",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "d94d8a87955031da2449",
+    "url": "/app/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "ee7e59101745cc07054b",
+    "url": "/app/static/css/main.dd3e5f8f.chunk.css"
+  },
+  {
+    "revision": "d94d8a87955031da2449",
+    "url": "/app/static/js/2.abedf867.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/app/static/js/2.abedf867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee7e59101745cc07054b",
+    "url": "/app/static/js/main.9f00380a.chunk.js"
+  },
+  {
+    "revision": "fb683d993c694aeb9312",
+    "url": "/app/static/js/runtime-main.95af9303.js"
+  }
+]);
