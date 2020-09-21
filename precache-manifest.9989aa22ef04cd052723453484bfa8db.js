d6e3e57c245cd842058766dd7057754ace4903c8
Rebecca Bol <rbol@cs.washington.edu>
2020-09-21 15:21:00 -0700

    Updates

---
+++ precache-manifest.9989aa22ef04cd052723453484bfa8db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8a16870cb3a1635a7ac4076a03fba5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00689db6beea5f2bf57c",
+    "url": "/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "87a041ecbe5d8ad8d796",
+    "url": "/static/css/main.0a73f9df.chunk.css"
+  },
+  {
+    "revision": "00689db6beea5f2bf57c",
+    "url": "/static/js/2.24c528c2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.24c528c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87a041ecbe5d8ad8d796",
+    "url": "/static/js/main.76176c74.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/app/precache-manifest.3a4db728a04bdcad68f1a77fab6b11ab.js"
+  "/precache-manifest.9989aa22ef04cd052723453484bfa8db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
